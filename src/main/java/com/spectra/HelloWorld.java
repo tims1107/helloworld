679f8a7af8f579064192deae96c3ba67ebe64bc1
tsmithers <tim.smithers@developerinc.com>
2019-10-17 16:14:34 -0400

    Initial commit.

---
+++ src/main/java/com/spectra/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.spectra;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class HelloWorld {
+    public static void main(String [] args){
+        System.out.println("hello world");
+        System.out.println(StringUtils.capitalize("hello world"));
+    }
+}
